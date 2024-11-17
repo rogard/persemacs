3b028fe76190f109dfd6c3a55e1685d709c58ce9
rogard <arobase.er@gmail.com>
2024-11-17 11:10:58 -0500

    minor changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,24 +1,33 @@
 ;; init.el --- Emacs init file
 ;; Attribution:
 ;; - https://github.com/ianyepan/yay-evil-emacs
-;; (setq package-enable-at-startup nil) ;; moved to early-init.el
-
+;; (setq package-enable-at-startup nil) ;; not sure about this
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 ;; (package-initialize) ;; Emacs calls package-initialize before evaluating your init file (since 27.1)
-
-;; workaround bug in Emacs 26.2
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3") ;; workaround bug in Emacs 26.2
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 (eval-and-compile
 (setq use-package-always-ensure t)) 
 ;; (setq use-package-always-defer t))
-
-(org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org")))
-
-(provide 'init)
+;; I tried default-directory, but ...
+(defcustom erw/routinel-directory "/home/erwann/github/rogard/routinel/" "where this package is located")
+(message "debug: %s" erw/routinel-directory)
+(let* ((remote-emacs-dir (expand-file-name ".emacs.d" erw/routinel-directory)))
+  (org-babel-load-file (expand-file-name "config.org" remote-emacs-dir)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
