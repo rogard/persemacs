c52d76f8f63994caeb1df49fe01188d6fbfad114
rogard <arobase.er@gmail.com>
2024-11-22 01:50:22 -0500

    Fixed bug in table-range

--- .emacs.d/extra.el
+++ .emacs.d/extra.el
@@ -19,7 +19,11 @@
      Credits: https://redd.it/r2nig7"
   (let ((file-name (or file-name (buffer-file-name (current-buffer)))))
     (with-current-buffer (find-file-noselect file-name)
-      (org-table-get-remote-range tbl-id range-address))))
+      (let ((result-with-properties
+             (org-table-get-remote-range tbl-id range-address)))
+        (mapcar (lambda (s)
+                  (substring-no-properties (substring s 1 -1)))
+                result-with-properties)))))
 (defalias 'erw/table-range 'erw/function-table-get-range-at-file)
   (defun erw/function-table-lookup (tbl-id key &optional file-name key-index value-index match-predicate)
     "Lookup field KEY-INDEX and return corresponding entry in field VALUE-INDEX from table TBL-ID."
@@ -32,16 +36,3 @@
             (value-range (erw/table-range tbl-id value-address file-name)))
         (org-lookup-first key key-range value-range 'string-match-p))))
 (defalias 'erw/table-lookup 'erw/function-table-lookup)
-(defun erw/table-to-path (tbl-name key &optional child)
-  (let* ((parent (erw/table-lookup tbl-name key))
-         (parent (erw/ensure-directory parent))
-         (args (if child
-                   (list child parent)
-                 (list parent))))
-    (message "debug: parent is %s" parent)
-    (apply #'expand-file-name args)))
-(defun erw/ensure-directory (path)
-  "Ensures PATH ends with a slash"
-  (if (not (string-match-p "/$" path))
-      (concat path "/")
-    path))
